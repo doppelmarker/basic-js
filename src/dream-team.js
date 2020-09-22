c8479669de98f62f17ea38e9ad196f368658724a
doppelmarker <doppelmarker@gmail.com>
2020-09-23 00:08:18 +0300

    feat: add solution to dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(membersArray) {
+  if (!Array.isArray(membersArray)) return false;
+
+  membersArray = membersArray.filter(member => {
+    if (typeof member === "string") {
+      return true;
+    } else return false;
+  });
+
+  let result = membersArray.reduce((a, member) => {
+      return a + member.trim()[0];
+  }, "");
+  
+  result = result.toUpperCase().split("").sort().join("");
+  return result;
 };
